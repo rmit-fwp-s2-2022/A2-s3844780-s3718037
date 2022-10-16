c52e66c6bf70b48bdcce8f57bddbb0ce19ab65c4
Michael Balkos <s3718037@student.rmit.edu.au>
2022-10-16 18:53:23 +1100

    User and thread GraphQL queries created

--- admin-dashboard/src/data/repository.js
+++ admin-dashboard/src/data/repository.js
@@ -5,6 +5,7 @@ const GRAPH_QL_URL = "http://localhost:4001/graphql";
 
 // --- Owner ---------------------------------------------------------------------------------------
 
+// Return all users
 async function getUsers() {
   // Simply query with no parameters.
   const query = gql`
@@ -21,11 +22,90 @@ async function getUsers() {
   `;
 
   const data = await request(GRAPH_QL_URL, query);
-
   return data.all_users;
 }
 
+// Return a single user based off userID
+async function getUser(userID) {
+  // Query with parameters (variables).
+  const query = gql`
+    query ($userID: Int) {
+      user(userID: $userID) {
+        userID,
+        email,
+        passwordHash,
+        profilePic,
+        createdAt,
+        updatedAt
+      }
+    }
+  `;
+
+  const variables = { userID };
+  const data = await request(GRAPH_QL_URL, query, variables);
+  return data.user;
+}
+
+async function updateUser(user) {
+  const query = gql`
+    mutation ($userID: Int, $name: String, $email: String, $passwordHash: String,
+      $profilePic: String, $createdAt: String, $updatedAt: String) {
+      update_user(input: {
+        userID: $userID,
+        name: $name,
+        email: $email,
+        passwordHash: passwordHash,
+        profilePic: profilePic,
+        createdAt: createdAt,
+        updatedAt: updatedAt
+      }) {
+        userID,
+        name,
+        email,
+        passwordHash,
+        profilePic,
+        createdAt,
+        updatedAt
+      }
+    }
+  `;
+
+  const variables = user;
+  const data = await request(GRAPH_QL_URL, query, variables);
+  return data.update_user;
+}
+
+async function updateThread(thread) {
+  const query = gql`
+    mutation ($threadID: Int, $post: String, $postPic: String,
+      $createdAt: String, $updatedAt: String, $userID: Int) {
+        update_thread(input: {
+        threadID: $threadID,
+        post: $post,
+        postPic: $postPic,
+        createdAt: createdAt,
+        updatedAt: updatedAt
+        userID: userID,
+        
+      }) {
+        threadID,
+        post,
+        postPic,
+        createdAt,
+        updatedAt,
+        userID
+      }
+    }
+  `;
+
+  const variables = thread;
+  const data = await request(GRAPH_QL_URL, query, variables);
+  return data.update_thread;
+}
+
+
+
 
 export {
-  getUsers
+  getUsers, getUser, updateUser, updateThread
 }
--- express-app/src/graphql/index.js
+++ express-app/src/graphql/index.js
@@ -55,9 +55,37 @@ graphql.schema = buildSchema(`
     userFollowedID: Int
   }
 
+  # The input type can be used for incoming data.
+  input ThreadInput {
+    threadID: Int,
+    post: String,
+    postPic: String,
+    createdAt: String,
+    updatedAt: String,
+    userID: Int
+  }
+
+  # The input type can be used for incoming data.
+  input UserInput {
+    userID: Int,
+    name: String,
+    email: String,
+    passwordHash: String,
+    profilePic: String,
+    createdAt: String,
+    updatedAt: String
+  }
+
+
   # Queries (read-only operations).
   type Query {
       all_users: [User]
+  }
+
+  # Mutations (modify data in the underlying data-source, i.e., the database).
+  type Mutation {
+    update_user(input: UserInput): User
+    update_thread(input: ThreadInput): Thread
   }
 
 `);
@@ -65,11 +93,56 @@ graphql.schema = buildSchema(`
 // The root provides a resolver function for each API endpoint.
 graphql.root = {
   // Queries.
+
+  // Return all users
   all_users: async () => {
     return await db.user.findAll();
+  },
+
+  // Return a single user based off userID
+  user: async (args) => {
+    return await db.user.findByPk(args.userID);
+  },
+
+  // Update user
+  update_user: async (args) => {
+    const user = await db.user.findByPk(args.input.userID);
+
+    // Updatefields.
+    user.userID = args.input.userID;
+    user.name = args.input.name;
+    user.email = args.input.email;
+    user.passwordHash = args.input.passwordHash;
+    user.profilePic = args.input.profilePic;
+    user.createdAt = args.input.createdAt;
+    user.updatedAt = args.input.updatedAt;
+
+    await user.save();
+    return user;
+  },
+
+  // Update thread
+  update_thread: async (args) => {
+    const thread = await db.thread.findByPk(args.input.threadID);
+
+    // Updatefields.
+    thread.threadID = args.input.threadID;
+    thread.post = args.input.post;
+    thread.postPic = args.input.postPic;
+    thread.createdAt = args.input.createdAt;
+    thread.updatedAt = args.input.updatedAt;
+    thread.userID = args.input.userID;
+
+    await thread.save();
+    return thread;
   }
+
+
+
 };
 
+
+
+
+
 module.exports = graphql;
-
-
