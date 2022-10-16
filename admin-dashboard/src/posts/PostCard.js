5a3da907e8ac35ed8892d54d7a6a57222c01237d
Michael Balkos <s3718037@student.rmit.edu.au>
2022-10-16 20:27:57 +1100

    Fix JSX 'className' instead of 'class'

--- admin-dashboard/src/posts/PostCard.js
+++ admin-dashboard/src/posts/PostCard.js
@@ -1,15 +1,15 @@
 
 export default function PostCard() {
 
-    
+
 
     return (
         <div className="col my-4">
-            <div class="card m-auto text-start" style={{ width: "20rem", minHeight: "300px" }}>
-                <div class="card-body">
-                    <h5 class="card-title">User Name</h5>
-                    <p class="card-text">A post message </p>
-                    <img src="https://i.imgur.com/MHKPi6F.jpeg" class="card-img-top" alt="..." />
+            <div className="card m-auto text-start" style={{ width: "20rem", minHeight: "300px" }}>
+                <div className="card-body">
+                    <h5 className="card-title">User Name</h5>
+                    <p className="card-text">A post message </p>
+                    <img src="https://i.imgur.com/MHKPi6F.jpeg" className="card-img-top" alt="..." />
                     <div className="mt-3">
                         <button className="btn btn-primary mx-4">Block User</button>
                         <button className="btn btn-danger">Remove Post</button>
--- admin-dashboard/src/users/DisplayUsers.js
+++ admin-dashboard/src/users/DisplayUsers.js
@@ -3,7 +3,7 @@ import UserCard from "./UserCard"
 export default function DisplayUsers() {
 
     return (
-        <div class="card m-auto text-start px-3 py-2 m-0 mt-5 border-none" style={{width: "68%"}}>
+        <div className="card m-auto text-start px-3 py-2 m-0 mt-5 border-none" style={{ width: "68%" }}>
             <UserCard />
             <UserCard />
             <UserCard />
